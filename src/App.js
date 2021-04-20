ed152e0deec806cbd16e994278bd6065a07a309b
Adriana Ito <ito.dri@gmail.com>
2021-04-20 16:08:54 +0900

    add components to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+import Header from './components/Header'
+import MemeGenerator from './components/MemeGenerator'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <h1>Hello</h1>
+        <MemeGenerator />
+      </div>
+    )
+  }
 }
 
 export default App;
