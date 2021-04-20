ccf45b48db2a2940e78a819a91e0932dbf671207
Adriana Ito <ito.dri@gmail.com>
2021-04-20 16:08:27 +0900

    add meme component

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    fetch()
+  }
+  render() {
+    return (
+      <div>Meme Generator section</div>
+    )
+  }
+}
+
+export default MemeGenerator
